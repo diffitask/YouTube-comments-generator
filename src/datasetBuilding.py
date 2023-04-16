92605475edbe99b699d5267eafb0e6d6a362982a
diffitask <ginger.ll@mail.ru>
2023-04-17 01:46:11 +0200

    add YouTube video info getting

--- src/datasetBuilding.py
+++ src/datasetBuilding.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import math
 from typing import Tuple
+from yt_dlp import YoutubeDL
 
 
 def build_dataset(path_to_data_file: str = "../data/UScomments.csv", ratio: float = 0.8) -> Tuple[pd.Series, pd.Series]:
@@ -19,3 +20,15 @@ def build_dataset(path_to_data_file: str = "../data/UScomments.csv", ratio: floa
     test_frame = dataframe[train_rows_cnt:]
 
     return train_frame, test_frame
+
+
+def get_youtube_video_info(youtube_video_id: str = "dQw4w9WgXcQ") -> Tuple[str, str]:
+    video_link = "https://www.youtube.com/watch?v=" + youtube_video_id
+
+    ydl = YoutubeDL({'outtmpl': '%(id)s.%(ext)s'})
+    with ydl:
+        info_dict = ydl.extract_info(video_link, download=False)
+        video_title = info_dict['title']
+        video_desc = info_dict['description']
+
+    return video_title, video_desc
