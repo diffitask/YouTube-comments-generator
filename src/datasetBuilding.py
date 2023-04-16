dac4c386e455a618f37419d9c6ee45bad56e4db3
diffitask <ginger.ll@mail.ru>
2023-04-16 23:32:13 +0200

    fix dataset cases with commas in quotes

--- src/datasetBuilding.py
+++ src/datasetBuilding.py
@@ -5,9 +5,12 @@ from typing import Tuple
 
 def build_dataset(path_to_data_file: str = "../data/UScomments.csv", ratio: float = 0.8) -> Tuple[pd.Series, pd.Series]:
     dataframe = pd.read_csv(path_to_data_file,
-                            sep='delimiter',
-                            engine='python',
+                            sep=',',
+                            quotechar='"',
+                            skipinitialspace=True,
+                            on_bad_lines='skip',
                             header=0)
+    dataframe = dataframe.drop(columns=["likes", "replies"])
 
     rows_cnt = len(dataframe)
     train_rows_cnt = int(math.ceil(ratio * rows_cnt))
